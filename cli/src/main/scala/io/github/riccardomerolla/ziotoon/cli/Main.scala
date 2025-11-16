0fd39f885fee18eabe30ff42216389f39e4300cc
Riccardo Merolla <riccardo.merolla@hey.com>
2025-11-16 11:20:10 +0100

    Bump CLI version to 0.2.0

--- cli/src/main/scala/io/github/riccardomerolla/ziotoon/cli/Main.scala
+++ cli/src/main/scala/io/github/riccardomerolla/ziotoon/cli/Main.scala
@@ -105,7 +105,7 @@ object Main extends ZIOAppDefault {
   private[cli] val cliApp: CliApp[ToonJsonService, String, Unit] =
     CliApp.make[ToonJsonService, String, CliAction, Unit](
       name = "zio-toon",
-      version = "0.1.0",
+      version = "0.2.0",
       summary = Span.text("TOON CLI"),
       command = encodeCommand orElse decodeCommand,
     ) {
